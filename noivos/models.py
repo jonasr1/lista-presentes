8d600a310f0c32a79d2c154178928e6a04523742
jonasr1 <yatosama924@gmail.com>
2024-11-20 10:15:54 -0300

    feat: adiciona modelo 'Presentes' e cria migração inicial

--- noivos/models.py
+++ noivos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Presentes(models.Model):
+    nome_presente = models.CharField(("Nome do Presente"),max_length=100)
+    foto = models.ImageField(("Foto"),upload_to='presentes')
+    preco = models.DecimalField(("Preço"), max_digits=6, decimal_places=2)
+    importancia = models.IntegerField(("Importância"))
+    reservado = models.BooleanField(default=False)
+    
+    def __str__(self) -> str:
+        return self.nome_presente
+    
