81a055b2f3e65a237b6ee47d212b612c8f66760c
jonasr1 <yatosama924@gmail.com>
2024-11-24 19:37:41 -0300

    feat(admin): configura Django Admin para modelos Convidados e Presentes

--- noivos/admin.py
+++ noivos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
+from noivos.models import Convidados, Presentes
+    
+@admin.register(Presentes)
+class PresenteAdmin(admin.ModelAdmin):
+    list_display = ('nome_presente', 'preco', 'foto', 'importancia')  # Configura as colunas visíveis
+    search_fields = ('nome_presente',)                                # Adiciona barra de pesquisa
+    list_filter = ('preco',)                                          # Adiciona filtros laterais
+    ordering = ('preco',)                                             # Define a ordem padrão de exibição
+    
+@admin.register(Convidados)
+class ConvidadosAdmin(admin.ModelAdmin):
+    list_display = ('nome_convidado', 'whatsapp', 'maximo_acompanhantes')
+    
