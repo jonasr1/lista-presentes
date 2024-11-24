f499776aba6f8f603a5cfa30b41aed0c5c1de3e2
jonasr1 <yatosama924@gmail.com>
2024-11-24 19:28:58 -0300

    style: adiciona estilos personalizados ao app noivos

---
+++ noivos/styles.py
@@ -0,0 +1,21 @@
+IMPORTANCIA_STYLES = {
+    'pouco_importante': {
+        'message': 'Importância moderada',
+        'class': 'text-gray-400',
+    },
+    'importante': {
+        'message': 'Importante',
+        'class': 'bg-orange-400/10 text-orange-400 ring-orange-400/30',
+        'icon_color': 'text-orange-400',
+    },
+    'muito_importante': {
+        'message': 'Muito Importante',
+        'class': 'bg-red-400/10 text-red-400 ring-red-400/30',
+        'icon_color': 'text-red-400',
+    },
+    'moderada': {
+        'message': 'Pouco importante',
+        'class': 'bg-green-400/10 text-green-400 ring-green-400/30',
+        'icon_color': 'text-green-400',
+    },
+}
